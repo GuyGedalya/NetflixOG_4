786031fd2a2c11d51267807dc709307a453c9b3f
Ofry <ofryhlely06@gmail.com>
2025-01-18 20:12:11 +0200

    fixed

---
+++ netflix_og/src/signIn/SignIn.js
@@ -0,0 +1,23 @@
+import './SignIn.css';
+
+function SignIn(){
+	return(
+		<div> 
+			<div className="logo-container">
+				<img src="/images/favicon.ico" alt="NOG Logo" className="logo"></img>
+			</div>
+			<div className="container">
+				<h1>Sign In</h1>
+				<form className="login-form">
+				<input type="email" id="email" name="email" placeholder="Email or mobile number" required/>
+			
+				<input type="password" id="password" name="password" placeholder="Password" required/>
+				<button type="submit" >Sign In</button>
+				</form>
+				<p>New to NOG? <a href="/signup.html">Sign up now.</a></p>
+			</div>
+	  </div>
+	);
+}
+
+export default SignIn;
