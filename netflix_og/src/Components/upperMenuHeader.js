74fc0c89803d569cef21edad858a05ac9d53bfbe
Ofry <ofryhlely06@gmail.com>
2025-01-23 23:06:26 +0200

    mode

--- netflix_og/src/Components/upperMenuHeader.js
+++ netflix_og/src/Components/upperMenuHeader.js
@@ -1,13 +1,41 @@
-import {React, useEffect, useState, useRef } from "react";
+import { React, useEffect, useState, useRef } from "react";
 import { useNavigate } from 'react-router-dom';
+import './upperMenu.css';
 
 function Header({ setResults, setShowSearchModal }) {
 	const [searchText, setSearchText] = useState('');
 	const [isOpen, setIsOpen] = useState(false);
 	const [isAdmin, setIsAdmin] = useState(false);
+	const [mode, setMode] = useState("Dark Mode");
+	const switchButtonRef = useRef('');
+
 
 	const navigate = useNavigate();
 	const searchBarRef = useRef(null);
+
+	// Change the theme of the page
+	useEffect(() => {
+		const header = document.querySelector("header");
+		header.classList.toggle("headerDark", mode === "Dark Mode");
+		header.classList.toggle("headerLight", mode === "Light Mode");
+
+		const buttons = document.querySelectorAll(".button");
+		buttons.forEach(button => {
+			button.classList.toggle("buttonDark", mode === "Dark Mode");
+			button.classList.toggle("buttonLight", mode === "Light Mode");
+		});
+		if (switchButtonRef.current) {
+			switchButtonRef.current.classList.toggle("buttonDark", mode === "Dark Mode");
+			switchButtonRef.current.classList.toggle("buttonLight", mode === "Light Mode");
+		}
+	}, [mode]);
+
+
+	const toggleMode = () => {
+		setMode((prevMode) =>
+			prevMode === "Dark Mode" ? "Light Mode" : "Dark Mode"
+		);
+	};
 
 	const checkAdmin = async () => {
 		const user = JSON.parse(sessionStorage.getItem("user"));
@@ -16,7 +44,7 @@ function Header({ setResults, setShowSearchModal }) {
 				method: 'GET'
 			});
 			if (response.ok) {
-				
+
 				const data = await response.json();
 				return data.Admin;
 			} else {
@@ -55,6 +83,13 @@ function Header({ setResults, setShowSearchModal }) {
 			setSearchText('');
 		}
 	};
+
+	const handleLogout = () => {
+		sessionStorage.removeItem('token');
+		sessionStorage.removeItem('user');
+		navigate('/');
+	}
+
 	useEffect(() => {
 		if (isOpen) {
 			// Listen to outside click event when search box is open
@@ -67,18 +102,20 @@ function Header({ setResults, setShowSearchModal }) {
 	}, [isOpen]);
 
 	useEffect(() => {
-        checkAdmin().then((result) => setIsAdmin(result));
-    }, []);
+		checkAdmin().then((result) => setIsAdmin(result));
+	}, []);
+
 
 	return (
-		<header>
+		<header >
 			<a href="/home" className="logo">
 				<img src="/images/favicon.ico" alt="Project Logo" className="logo-image" />
 			</a>
-			<nav>
+			<nav >
 				<button onClick={() => (window.location.href = '/home')} className='header button'>Home</button>
 				<button onClick={() => navigate('/categories')} className='header button'>Categories</button>
 				{isAdmin && <button onClick={() => navigate('/Manage')}>Manage</button>}
+
 				{!isOpen ? (<button onClick={() => setIsOpen(true)} className='header button'>Search</button>) :
 					(<div className="search-bar" ref={searchBarRef}>
 						<input
@@ -96,6 +133,8 @@ function Header({ setResults, setShowSearchModal }) {
 						/>
 					</div>
 					)}
+				<button ref={switchButtonRef} onClick={toggleMode}>Switch to {mode === "Dark Mode" ? "Light" : "Dark"}</button>
+				<button onClick={handleLogout} className='header button'>Log Out</button>
 			</nav>
 		</header>
 	);
