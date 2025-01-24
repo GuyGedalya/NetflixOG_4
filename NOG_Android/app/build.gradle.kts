0921324b4e0fdc350fa5ff7c8e53f634cbebb02f
Ofry <ofryhlely06@gmail.com>
2025-01-25 01:54:33 +0200

    כיףכיףכיף

--- NOG_Android/app/build.gradle.kts
+++ NOG_Android/app/build.gradle.kts
@@ -37,7 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.room.common)
+    implementation("androidx.room:room-runtime:2.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
 }
---
+++ NOG_Android/app/src/main/java/com/example/nog_android/Category.java
@@ -0,0 +1,34 @@
+package com.example.nog_android;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import com.google.gson.annotations.SerializedName;
+
+@Entity
+public class Category {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("promoted")
+    private boolean promoted;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPromoted() {
+        return promoted;
+    }
+
+    public void setPromoted(boolean promoted) {
+        this.promoted = promoted;
+    }
+}
