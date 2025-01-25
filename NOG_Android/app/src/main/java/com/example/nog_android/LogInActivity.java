98c58983f40dd88ecc571fa9cb1cf9bbeaa0e391
Ofry <ofryhlely06@gmail.com>
2025-01-25 19:13:27 +0200

    screens for sign up and log in are done

--- NOG_Android/app/src/main/java/com/example/nog_android/LogInActivity.java
+++ NOG_Android/app/src/main/java/com/example/nog_android/LogInActivity.java
@@ -1,6 +1,8 @@
 package com.example.nog_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Button toSignUp = findViewById(R.id.toSignUpBtn);
+        toSignUp.setOnClickListener(v-> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- NOG_Android/app/src/main/java/com/example/nog_android/SignupActivity.java
+++ NOG_Android/app/src/main/java/com/example/nog_android/SignupActivity.java
@@ -1,6 +1,8 @@
 package com.example.nog_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Button toLogInBtn = findViewById(R.id.toLogInBtn);
+
+        toLogInBtn.setOnClickListener( v -> {
+            Intent intent = new Intent(this, LogInActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
